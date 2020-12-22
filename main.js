1b13e17201dd46118bbe2cb8562e92b20c25fdb1
Mathieu Colmon <mathieu@usp-3.fr>
2020-12-22 01:32:10 +0100

    Lint + Optimizations

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ const fs = require('fs');
 
 const modules = [];
 
-fs.readdir('./modules', (err, modules_files) => {
-  modules_files.forEach(mod_file => {
+fs.readdir('./modules', (_, modules_files) => {
+  modules_files.forEach((mod_file) => {
+    if (!fs.lstatSync(`./modules/${mod_file}`).isFile()) return;
     modules.push(require(`./modules/${mod_file}`));
   });
 });
 
-ioHook.on('keypress', e => {
-  modules.forEach(module => {
-    module.shortcuts.forEach(shortcut => {
+let lastPress = null;
+ioHook.on('keypress', (e) => {
+  const newPress = Object.values(e).toString();
+  if (lastPress === newPress) return;
+  lastPress = newPress;
+
+  modules.forEach((module) => {
+    if (!module.shortcuts) return;
+    module.shortcuts.forEach((shortcut) => {
       let valid = true;
-      Object.keys(shortcut).forEach(condition => {
+      Object.keys(shortcut).forEach((condition) => {
         if (e[condition] !== shortcut[condition]) valid = false;
       });
       if (valid) module.handle(e);
     });
   });
+  setTimeout(() => lastPress = null, 200);
 });
 
 ioHook.start();
