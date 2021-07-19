6f05a4eefe3e19c7d116e56d92354ef88e082227
Mathieu Colmon <mathieu@usp-3.fr>
2021-07-19 03:30:20 +0200

    CPU and Memory usage report

--- modules/pc.js
+++ modules/pc.js
@@ -1,4 +1,19 @@
 const { exec } = require('child_process');
+const os = require('os');
+const cpu = require('./libs/cpuUsage');
+
+const usage = { cpu: 0, mem: 0 };
+const totalMem = os.totalmem();
+
+setInterval(() => {
+  cpu.cpuUsage((val) => {
+    usage.cpu = Math.round(val * 100);
+  });
+
+  usage.mem = Math.round(((totalMem - os.freemem()) / totalMem) * 100);
+
+  global.tcpSend(`pc:usage,${usage.cpu},${usage.mem}`);
+}, 1000);
 
 global.tcpListen((packet) => {
   if (packet === 'pcAction:shutdown') exec('rundll32.exe powrprof.dll,SetSuspendState 0,0,0');
